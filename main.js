a4f507de8d07033ea1d2fb96de810b840cd05373
Jose Castro <jf.castro973@gmail.com>
2023-03-22 21:26:47 +0100

    varias pantallas más integradas!

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail     =   document.querySelector('.navbar-email');
+const desktopMenu   =   document.querySelector('.desktop-menu');
+const menuHamIcon   =   document.querySelector('.menu');
+const mobileMenu    =   document.querySelector('.mobile-menu');
+const menuCarrito   =   document.querySelector('.navbar-shopping-cart')
+const allCarrito    =   document.querySelector('.product-detail')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click',toggleCarrito);
 
 function toggleDesktopMenu () {
-    console.log ('click')
     desktopMenu.classList.toggle ('inactive')
+    allCarrito.classList.add ('inactive')
+    
 }
 
-console.log('Js funcionando')
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle ('inactive')
+    allCarrito.classList.add ('inactive')
+}
+
+function toggleCarrito () {
+    allCarrito.classList.toggle ('inactive')
+    desktopMenu.classList.add ('inactive')
+    mobileMenu.classList.add ('inactive')
+}
